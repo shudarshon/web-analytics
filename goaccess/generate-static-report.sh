e3a1c9eae7057ee88b68d8434fedae6144630fad
shudarshon <shudarshon@upskillable.com>
2018-09-04 23:33:50 +0600

    added bash script to generate static html report

--- goaccess/generate-static-report.sh
+++ goaccess/generate-static-report.sh
@@ -6,5 +6,11 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 #  this script will fetch webserver log from S3 bucket and parse them goaccess which will generate static html file
 #  nginx will serve this static content finally delivering a beautiful dashboard
 ##
-sudo aws s3 sync s3://website-log-bucket/ /var/log/website-log/
-sudo zcat -f /var/log/website-log/access.*.gz | sudo goaccess -a --ignore-crawlers > /var/www/html/index.html
+
+sudo /usr/bin/aws s3 sync s3://website-log-bucket/ /var/log/website-log/
+sudo /bin/zcat -f /var/log/upskillable-log/access.*.gz | sudo /usr/local/bin/goaccess -a --ignore-crawlers -o /var/www/html/index.html - > /dev/null 2>&1
+
+##
+# set a aronjob as following
+# */15 * * * * /usr/local/bin/generate-static-report.sh > /dev/null 2>&1
+#
